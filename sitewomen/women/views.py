6132c1055b7e8b2f358833f942adeb85e85e8da9
Dimitry <tizya.noonie@gmail.com>
2025-03-02 14:57:38 +0300

    Completed step 1.5 on stepik.org

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Main mage of \'women\' application')
+
+
+def categories(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('<h1>Articles by category</h1>')
