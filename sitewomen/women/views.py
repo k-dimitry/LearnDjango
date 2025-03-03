a82ccdf6458ca3fe119d408ecd2150ad0d6b6a29
Dimitry <tizya.noonie@gmail.com>
2025-03-03 15:42:19 +0300

    Completed step 1.6 on stepik.org

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('Main mage of \'women\' application')
+    return HttpResponse("Main mage of 'women' application")
 
 
-def categories(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>Articles by category</h1>')
+def categories(request: HttpRequest, cat_id: int) -> HttpResponse:
+    return HttpResponse(f'<h1>Articles by category</h1><p>id: {cat_id}</p>')
+
+
+def categories_by_slug(request: HttpRequest, cat_slug: str) -> HttpResponse:
+    return HttpResponse(f'<h1>Articles by category</h1><p>slug: {cat_slug}</p>')
+
+
+def archive(request: HttpRequest, year: int) -> HttpResponse:
+    return HttpResponse(f'<h1>Archive by year</h1><p>{year}</p>')
