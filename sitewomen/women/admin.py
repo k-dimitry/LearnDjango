8b08021dc176ff4dc0bb2746d9c49196815dabd2
Dimitry <tizya.noonie@gmail.com>
2025-03-23 14:48:45 +0300

    Completed step 6.3 on stepik.org

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,14 +1,33 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models import QuerySet
+from django.db.models.functions import Length
+from django.http import HttpRequest
+
 from .models import Woman, Category
 
 
 @admin.register(Woman)
 class WomanAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_created', 'is_published', 'cat')
-    list_display_links = ('id', 'title')
+    list_display = ('title', 'time_created', 'is_published', 'cat', 'brief_info')
+    list_display_links = ('title',)
     ordering = ('-time_created', 'title')
     list_editable = ('is_published',)
     list_per_page = 5
+    actions = ('set_published', 'set_draft')
+
+    @admin.display(description='Brief Info', ordering=Length('content'))
+    def brief_info(self, woman: Woman) -> str:
+        return f'Description: {len(woman.content)} symbols.'
+
+    @admin.display(description='Publish selected Famous women')
+    def set_published(self, request: HttpRequest, queryset: QuerySet):
+        count = queryset.update(is_published=Woman.Status.PUBLISHED)
+        self.message_user(request, message=f'{count} posts were changed.')
+
+    @admin.display(description='Change status to "Draft"')
+    def set_draft(self, request: HttpRequest, queryset: QuerySet):
+        count = queryset.update(is_published=Woman.Status.DRAFT)
+        self.message_user(request, message=f'{count} posts were changed to "draft".', level=messages.WARNING)
 
 
 @admin.register(Category)
