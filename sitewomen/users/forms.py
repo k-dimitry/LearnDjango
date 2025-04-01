748c7ad949430facea908f874dfaeebe18587287
Dimitry <tizya.noonie@gmail.com>
2025-04-01 23:02:33 +0300

    Completed step 9.6 on stepik.org

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -14,3 +14,30 @@ class LoginUserForm(AuthenticationForm):
     class Meta:
         model = get_user_model()
         fields = ('username', 'password')
+
+
+class RegisterUserForm(forms.ModelForm):
+    username = forms.CharField(label='Login')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput())
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'password2')
+        labels = {
+            'email': 'E-mail',
+            'first_name': 'Name',
+            'last_name': 'Last name',
+        }
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords do not match')
+        return cd['password']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if get_user_model().objects.filter(email=email).exists():
+            raise forms.ValidationError('E-mail already in use')
+        return email
--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -4,24 +4,7 @@ from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 
-from .forms import LoginUserForm
-
-
-# def login_user(request: HttpRequest) -> HttpResponse:
-#     if request.method == 'POST':
-#         form = LoginUserForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request, username=cd['username'], password=cd['password'])
-#             if user and user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse('home'))
-#     else:
-#         form = LoginUserForm()
-#     context = {
-#         'form': form
-#     }
-#     return render(request, template_name='users/login.html', context=context)
+from .forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -34,6 +17,15 @@ class LoginUser(LoginView):
     # def get_success_url(self):
     #     return reverse_lazy('home')
 
-# def logout_user(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse('users:login'))
+
+def register(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, template_name='users/register_done.html')
+    else:
+        form = RegisterUserForm()
+    return render(request, template_name='users/register.html', context={'form': form})
