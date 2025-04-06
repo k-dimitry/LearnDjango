04f639936db9cdae93e0d813ad6823fd4b441692
Dimitry <tizya.noonie@gmail.com>
2025-04-06 16:34:08 +0300

    Completed step 10.2 on stepik.org

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -162,3 +162,19 @@ EMAIL_ADMIN = EMAIL_HOST_USER
 
 AUTH_USER_MODEL = 'users.User'
 DEFAULT_USER_IMAGE = MEDIA_URL + 'users/default.png'
+
+SOCIAL_AUTH_GITHUB_KEY = 'Ov23lihOWgoNyOMr1odR'
+SOCIAL_AUTH_GITHUB_SECRET = '13a4e3f0d85e1edcdc0a082b7b1885ccf8c085c6'
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'users.pipeline.new_users_handler',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
