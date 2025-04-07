7c40e020b960cc728bcb8b0396d91699658f3fa6
dimitry <tizya.noonie@gmail.com>
2025-04-07 19:30:34 +0300

    Completed step 12.2 on stepik.org: Added tests form main page.

--- sitewomen/women/tests.py
+++ sitewomen/women/tests.py
@@ -3,8 +3,12 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
+from women.models import Woman
+
 
 class GetPagesTestCase(TestCase):
+    fixtures = ('women_woman.json', 'women_category.json', 'women_husband.json', 'women_tagpost.json')
+
     def setUp(self):
         """Initialization before each test"""
 
@@ -16,15 +20,34 @@ class GetPagesTestCase(TestCase):
         self.assertTemplateUsed(response, 'women/index.html')
         self.assertEqual(response.context_data['title'], 'Main Page')
 
-    def text_redirect_addpage(self):
+    def test_redirect_addpage(self):
         path = reverse('add_page')
         redirect_uri = reverse('users:login') + '?next=' + path
         response = self.client.get(path)
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertRedirects(response, redirect_uri)
 
-    def text_case_2(self):
-        pass
+    def test_data_mainpage(self):
+        w = Woman.published.all().select_related('cat')
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertQuerySetEqual(response.context_data['posts'], w[:5])
+
+    def test_paginate_mainpage(self):
+        path = reverse('home')
+        page = 2
+        paginate_by = 5
+
+        response = self.client.get(path + f'?page={page}')
+        w = Woman.published.all().select_related('cat')
+
+        self.assertQuerySetEqual(response.context_data['posts'], w[(page - 1) * paginate_by: page * paginate_by])
+
+    def test_content_post(self):
+        w = Woman.published.get(pk=1)
+        path = reverse('post', args=[w.slug])
+        response = self.client.get(path)
+        self.assertEqual(w.content, response.context_data['post'].content)
 
     def tearDown(self):
         """Actions after each test"""
