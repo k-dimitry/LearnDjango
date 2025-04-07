d82f5c3b0e7c6f62f3f0ec4a362a21dc50b8cc38
dimitry <tizya.noonie@gmail.com>
2025-04-07 18:53:03 +0300

    Completed step 12.1 on stepik.org: Added test using Django unittest.

--- sitewomen/women/tests.py
+++ sitewomen/women/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class GetPagesTestCase(TestCase):
+    def setUp(self):
+        """Initialization before each test"""
+
+    def test_mainpage(self):
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        # self.assertIn('women/index.html', response.template_name)
+        self.assertTemplateUsed(response, 'women/index.html')
+        self.assertEqual(response.context_data['title'], 'Main Page')
+
+    def text_redirect_addpage(self):
+        path = reverse('add_page')
+        redirect_uri = reverse('users:login') + '?next=' + path
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, redirect_uri)
+
+    def text_case_2(self):
+        pass
+
+    def tearDown(self):
+        """Actions after each test"""
